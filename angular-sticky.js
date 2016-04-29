644248a8f026dc31b6bca5ea2090329db2e9a14e
Maelig GOHIN <mgohin@arca-computing.fr>
2016-04-29 16:39:11 +0200

    fix query selector

--- angular-sticky.js
+++ angular-sticky.js
@@ -33,7 +33,7 @@
                     angular.element(scrolling).on('scroll', function () {
                         var offsetTop = elem[0].getBoundingClientRect().top;
                         var offsetKeepVisible = 0;
-                        angular.forEach(angular.element(document.querySelector('.sticky-wrapper.sticky-keep-visible')), function (elem) {
+                        angular.forEach(angular.element(document.querySelectorAll('.sticky-wrapper.sticky-keep-visible')), function (elem) {
                             var jThis = angular.element(elem);
                             if (jThis !== clone && jThis[0].offsetWidth > 0 && jThis[0].offsetHeight > 0) {
                                 offsetKeepVisible += jThis[0].offsetHeight;
@@ -42,7 +42,7 @@
 
                         if (offsetTop <= offsetKeepVisible) {
                             if (!visible) {
-                                angular.element(document.querySelector('.sticky-wrapper:not(.sticky-keep-visible')).css('display', 'none');
+                                angular.element(document.querySelectorAll('.sticky-wrapper:not(.sticky-keep-visible')).css('display', 'none');
                                 clone.css('top', offsetKeepVisible + 'px');
                                 clone.css('display', 'block');
                                 visible = true;
